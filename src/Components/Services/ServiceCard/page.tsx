a1a2331eef73d538c4008a7486a3633694190ef7
kaiqueq147 <kaiqueq147@gmail.com>
2023-09-08 10:12:49 -0300

    small-fixes-padding-right

--- src/Components/Services/ServiceCard/page.tsx
+++ src/Components/Services/ServiceCard/page.tsx
@@ -7,7 +7,7 @@ export function ServiceCard({ title, description }: ServiceCardProps) {
   return (
     <div
       id="ServiceCard"
-      className="w-[100%] h-[20.5rem] my-7  pl-12 pt-14 bg-[#1A2E35] rounded-md shadow-xl "
+      className="w-[100%] h-[20.5rem] my-7  pl-12 pr-2 pt-14 bg-[#1A2E35] rounded-md shadow-xl "
     >
       <img src="http://localhost:3000/icone.svg" className="w-16 " />
       <div>
