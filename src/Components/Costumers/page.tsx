7ae74758e4fe7b762136aead289b0c7349489ac1
kaiqueq147 <kaiqueq147@gmail.com>
2024-03-05 14:08:02 -0300

    try-fix-images

--- src/Components/Costumers/page.tsx
+++ src/Components/Costumers/page.tsx
@@ -10,6 +10,7 @@ export function Costumers() {
     "/logo-branca.png",
     "/logo-branca.png",
   ];
+
   return (
     <div
       id="costumer-container"
@@ -23,34 +24,26 @@ export function Costumers() {
           At half-past eight the door opened, the policeman appeared, and,
           requesting them to follow him, led the way to an adjoining hall.
         </p>
-        <div className="w-[100vw]">
+        <div className="w-full">
           <Carousel
             visibleButtonControl={false}
             loop={true}
             align={"end"}
             visibleDots={true}
           >
-            {images.map((src, i) => {
-              return (
-                // 👇 style each individual slide.wq2
-                // flex[0_0_100%]
-                //   - shorthand for flex-grow:0; flex-shrink:0; flex-basis:100%
-                //   - we want this slide to not be able to grow or shrink and take up 100% width of the viewport.
-                <div
-                  className="relative h-16 my-12 flex-[0_0_25%] max-xl:flex-[0_0_50%] max-[1350px]:flex-[0_0_35%] max-lg:flex-[0_0_100%]"
-                  key={i}
-                >
-                  {/* use object-cover + fill since we don't know the height and width of the parent */}
-                  <Image
-                    src={src}
-                    key={i}
-                    fill
-                    className="object-contain px-4"
-                    alt="alt"
-                  />
-                </div>
-              );
-            })}
+            {images.map((src, i) => (
+              <div
+                className="relative h-16 my-12 flex-[0_0_25%] max-xl:flex-[0_0_50%] max-[1350px]:flex-[0_0_35%] max-lg:flex-[0_0_100%]"
+                key={i}
+              >
+                <Image
+                  src={src}
+                  layout="fill"
+                  objectFit="contain"
+                  alt={`Logo ${i}`}
+                />
+              </div>
+            ))}
           </Carousel>
         </div>
       </div>
