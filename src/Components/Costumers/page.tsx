c708cf435127cc228d3065ff1266f7b6fdd5db09
kaiqueq147 <kaiqueq147@gmail.com>
2024-03-05 17:29:02 -0300

    back-default

--- src/Components/Costumers/page.tsx
+++ src/Components/Costumers/page.tsx
@@ -10,11 +10,6 @@ export function Costumers() {
     "/logo-branca.png",
     "/logo-branca.png",
   ];
-
-  // const imageSlides = images.map((src, i) => (
-
-  // ));
-
   return (
     <div
       id="costumer-container"
@@ -35,38 +30,27 @@ export function Costumers() {
             align={"end"}
             visibleDots={true}
           >
-            <div className="relative h-16 my-12 flex-[0_0_25%] max-xl:flex-[0_0_50%] max-[1350px]:flex-[0_0_35%] max-lg:flex-[0_0_100%]">
-              <Image
-                src="/logo-branca.png"
-                fill
-                className="object-contain px-4"
-                alt={"/logo-branca.png"}
-              />
-            </div>
-            <div className="relative h-16 my-12 flex-[0_0_25%] max-xl:flex-[0_0_50%] max-[1350px]:flex-[0_0_35%] max-lg:flex-[0_0_100%]">
-              <Image
-                src="/logo-branca.png"
-                fill
-                className="object-contain px-4"
-                alt={"/logo-branca.png"}
-              />
-            </div>
-            <div className="relative h-16 my-12 flex-[0_0_25%] max-xl:flex-[0_0_50%] max-[1350px]:flex-[0_0_35%] max-lg:flex-[0_0_100%]">
-              <Image
-                src="/logo-branca.png"
-                fill
-                className="object-contain px-4"
-                alt={"/logo-branca.png"}
-              />
-            </div>
-            <div className="relative h-16 my-12 flex-[0_0_25%] max-xl:flex-[0_0_50%] max-[1350px]:flex-[0_0_35%] max-lg:flex-[0_0_100%]">
-              <Image
-                src="/logo-branca.png"
-                fill
-                className="object-contain px-4"
-                alt={"/logo-branca.png"}
-              />
-            </div>
+            {images.map((src, i) => {
+              return (
+                // 👇 style each individual slide.wq2
+                // flex[0_0_100%]
+                //   - shorthand for flex-grow:0; flex-shrink:0; flex-basis:100%
+                //   - we want this slide to not be able to grow or shrink and take up 100% width of the viewport.
+                <div
+                  className="relative h-16 my-12 flex-[0_0_25%] max-xl:flex-[0_0_50%] max-[1350px]:flex-[0_0_35%] max-lg:flex-[0_0_100%]"
+                  key={i}
+                >
+                  {/* use object-cover + fill since we don't know the height and width of the parent */}
+                  <Image
+                    src={src}
+                    key={i}
+                    fill
+                    className="object-contain px-4"
+                    alt={src}
+                  />
+                </div>
+              );
+            })}
           </Carousel>
         </div>
       </div>
