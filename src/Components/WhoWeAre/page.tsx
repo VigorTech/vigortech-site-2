c0b0b302cb2f8c1561a1d727f42db5331642c520
kaiqueq147 <kaiqueq147@gmail.com>
2024-03-06 13:30:15 -0300

    test

--- src/Components/WhoWeAre/page.tsx
+++ src/Components/WhoWeAre/page.tsx
@@ -78,34 +78,33 @@ export function WhoWeAre({ language }: WhoWeAreProps) {
         flex[0_0_100%] // - shorthand for flex-grow:0; flex-shrink:0;
         flex-basis:100% // - we want this slide to not be able to grow or shrink
         and take up 100% width of the viewport.
-        <div className="relative h-56 my-12 flex-[0_0_25%] max-xl:flex-[0_0_50%] max-[1350px]:flex-[0_0_35%] max-lg:flex-[0_0_100%]">
-          {/* use object-cover + fill since we don't know the height and width of the parent */}
-          <Image
-            src="/background.png"
-            fill
-            className="object-contain px-4"
-            alt="alt1"
-          />
-          <Image
-            src="/background.png"
-            fill
-            className="object-contain px-4"
-            alt="alt1"
-          />
-          <Image
-            src="/background.png"
-            fill
-            className="object-contain px-4"
-            alt="alt1"
-          />
-          <Image
-            src="/background.png"
-            fill
-            className="object-contain px-4"
-            alt="alt1"
-          />
-        </div>
+        {/* use object-cover + fill since we don't know the height and width of the parent */}
+        <Image
+          src="/background.png"
+          fill
+          className="object-contain px-4"
+          alt="alt1"
+        />
+        <Image
+          src="/background.png"
+          fill
+          className="object-contain px-4"
+          alt="alt1"
+        />
+        <Image
+          src="/background.png"
+          fill
+          className="object-contain px-4"
+          alt="alt1"
+        />
+        <Image
+          src="/background.png"
+          fill
+          className="object-contain px-4"
+          alt="alt1"
+        />
       </div>
+
       {/* <Carousel
         visibleButtonControl={true}
         loop={true}
